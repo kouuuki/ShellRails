745c72b8da8adddf02bf2cd07fdd0dc6f6f4fc77
kouuuki <mark.web.888@gmail.com>
2017-11-29 12:43:12 +0900

    gemfileを設定できるように変更

---
+++ rails.sh
@@ -0,0 +1,59 @@
+rails new AppName -b
+cd AppName
+# gemfileの作成
+cat << EOF > Gemfile
+source 'https://rubygems.org'
+
+group :default do
+  gem 'rails'
+  gem 'sass-rails'
+  gem 'uglifier'
+  gem 'coffee-rails'
+
+  gem 'active_model_serializers'
+end
+
+group :test, :development do
+  gem 'sqlite3'
+end
+
+group :development do
+  gem 'spring'
+  gem 'spring-commands-rspec'
+
+  gem 'pry-rails'
+  gem 'pry-doc'
+  gem 'pry-stack_explorer'
+  gem 'pry-byebug'
+
+  gem 'guard'
+  gem 'guard-rspec'
+  gem 'guard-spring'
+
+  gem 'better_errors'
+  gem 'binding_of_caller'
+
+  gem 'rack-mini-profiler'
+  gem 'bullet'
+  gem 'quiet_assets'
+  gem 'rails_best_practices'
+  gem 'did_you_mean'
+
+  gem 'annotate'
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'database_rewinder'
+  gem 'rspec-request_describer'
+  gem 'autodoc'
+  gem 'json_spec'
+end
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', '~> 0.4.0'
+end
+EOF
+# gemfileの作成
